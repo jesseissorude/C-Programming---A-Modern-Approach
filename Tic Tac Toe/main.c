feee9e5810435d8b307800ea5c567e705d131263
jesseissorude <jesse.rhew@gmail.com>
2024-04-06 19:26:49 -0500

    tic tac toe printing the blank board

--- Tic Tac Toe/main.c
+++ Tic Tac Toe/main.c
@@ -3,13 +3,28 @@
 
 #define PI 3.14159625
 
+char space[3][3];
+
+void printBoard();
+void createBoard();
+
 int main () {
-  double amount;
+    createBoard();
+    printBoard();
+}
 
-  printf("Enter a dollar amount: ");
-  scanf("%lf", &amount);
+void printBoard() {
+    printf(" %c | %c | %c \n", space[0][0], space[0][1], space[0][2]);
+    printf("---|---|---\n");
+    printf(" %c | %c | %c \n", space[1][0], space[1][1], space[1][2]);
+    printf("---|---|---\n");
+    printf(" %c | %c | %c \n", space[2][0], space[2][1], space[2][2]);
+}
 
-  amount = amount + (0.05 * amount);
-
-  printf("With tax added: %.2lf", amount);
+void createBoard() {
+    for (int i=0; i<3; i++) {
+        for (int j=0; j<3; j++) {
+            space[i][j] = ' ';
+        }
+    }
 }
