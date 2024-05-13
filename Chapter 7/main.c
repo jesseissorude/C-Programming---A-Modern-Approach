7392513c0cf804c94f229d798077fa5116d5a65d
jesseissorude <jesse.rhew@gmail.com>
2024-05-13 17:51:15 -0500

    tried to ring the bell ;-;

--- Chapter 7/main.c
+++ Chapter 7/main.c
@@ -1,16 +1,5 @@
 #include <stdio.h>
-#include <limits.h>
 
 int main (void) {
-    unsigned int x = UINT_MAX;
-
-    printf("x : %u\n", x);
-    x += 1;
-    printf("x : %u\n", x);
-    printf("x : 0x%08x\n", x);
-    x = x + 1;
-    printf("x : 0x%08x\n", x);
-
-    
-
+    printf("\a");
 }
