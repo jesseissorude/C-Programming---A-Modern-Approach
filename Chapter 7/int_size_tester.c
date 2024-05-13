0e74e42d315a3027034190c76fe65cca26c22166
jesseissorude <jesse.rhew@gmail.com>
2024-05-13 15:47:34 -0500

    renamed scratch file to int_size_tester.c to keep around to use on microcontrollers someday

--- Chapter 7/int_size_tester.c
+++ Chapter 7/int_size_tester.c
@@ -2,6 +2,9 @@
     int
     long int
     short int
+
+    Update: Now that it's done, I could port this over (if necessary) and 
+    run it on other devices (especially interesting on microcrontrollers)
 */
 
 #include <stdio.h>
